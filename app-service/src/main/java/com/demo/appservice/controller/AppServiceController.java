d141cf019d1954561ae477ee6d1fbdd30d8c97d7
bqchen <chen_905@yeah.net>
2019-07-12 13:18:52 +0800

    update

--- app-service/src/main/java/com/demo/appservice/controller/AppServiceController.java
+++ app-service/src/main/java/com/demo/appservice/controller/AppServiceController.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -17,37 +18,42 @@ import java.util.List;
 @RestController
 public class AppServiceController {
 
-    @Autowired
-    @Qualifier("eurekaRegistration")
-    private Registration registration;
-
-    @Autowired
-    private DiscoveryClient client;
-
-    @Value("${msg:unknown}")
-    private String msg;
-
-    @GetMapping(value = "/")
-    public String printServiceB() {
-        ServiceInstance serviceInstance = serviceInstance();
-        return serviceInstance.getServiceId() + " (" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + ")" + "===> Say " + msg;
+    @RequestMapping("/home")
+    public String home() {
+        return "Welcome to the Spring Cloud Azure DevOps World!";
     }
 
-    public ServiceInstance serviceInstance() {
-        List<ServiceInstance> list = client.getInstances(registration.getServiceId());
-        if (list != null && list.size() > 0) {
-            for (ServiceInstance item : list) {
-                return item;
-            }
-        }
-        return null;
-    }
-
-
-    @GetMapping(path = "/current")
-    public Principal getCurrentAccount(Principal principal) {
-        return principal;
-    }
+//    @Autowired
+//    @Qualifier("eurekaRegistration")
+//    private Registration registration;
+//
+//    @Autowired
+//    private DiscoveryClient client;
+//
+//    @Value("${msg:unknown}")
+//    private String msg;
+//
+//    @GetMapping(value = "/")
+//    public String printServiceB() {
+//        ServiceInstance serviceInstance = serviceInstance();
+//        return serviceInstance.getServiceId() + " (" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + ")" + "===> Say " + msg;
+//    }
+//
+//    public ServiceInstance serviceInstance() {
+//        List<ServiceInstance> list = client.getInstances(registration.getServiceId());
+//        if (list != null && list.size() > 0) {
+//            for (ServiceInstance item : list) {
+//                return item;
+//            }
+//        }
+//        return null;
+//    }
+//
+//
+//    @GetMapping(path = "/current")
+//    public Principal getCurrentAccount(Principal principal) {
+//        return principal;
+//    }
 
 //    @Value("${server.port}")
 //    String port;
