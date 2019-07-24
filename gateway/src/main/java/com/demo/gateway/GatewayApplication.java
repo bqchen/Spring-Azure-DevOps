72fcb20ed1b3e699603c8ca2b847f4812871ddbc
bqchen <chen_905@yeah.net>
2019-07-24 14:09:40 +0800

    update

--- gateway/src/main/java/com/demo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/demo/gateway/GatewayApplication.java
@@ -6,10 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableDiscoveryClient
-@EnableEurekaClient
-@SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
--- registry/src/main/java/com/demo/registry/RegistryApplication.java
+++ registry/src/main/java/com/demo/registry/RegistryApplication.java
@@ -2,10 +2,8 @@ package com.demo.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
 @EnableEurekaServer
 @SpringBootApplication
 public class RegistryApplication {
